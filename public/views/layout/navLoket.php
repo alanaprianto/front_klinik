81d5d20851bbed53733e9b692bb8f971e76b6f0e
Lucky Lukman Alamsah <key.lukman@gmail.com>
2017-05-04 16:03:23 +0700

    fix navLoket in layout

--- public/views/layout/navLoket.php
+++ public/views/layout/navLoket.php
@@ -19,7 +19,14 @@
                                     <span class="sidebar-nav-item-icon fa fa-address-book fa-fw"></span>
                                     <span class="sidebar-nav-item">Pendaftaran</span>
                                 </a>
-                            </li>                           
+                            </li>
+                            <li>
+                                <a href="dataPasien">
+                                    <span class="sidebar-nav-item-icon fa fa-male"></span>
+                                    <span class="sidebar-nav-item">Pengunjung</span>
+                                </a>
+                            </li>
+                           
                             <li class="sidebar-nav-heading">Lain-lain</li>
                             <li>
                                 <a href="login">
