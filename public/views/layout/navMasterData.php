8d48d2eedca2960cf293c11f8cd46bb38e8ae717
Lucky Lukman Alamsah <key.lukman@gmail.com>
2017-05-09 16:00:18 +0700

    nav masterdata

---
+++ public/views/layout/navMasterData.php
@@ -0,0 +1,29 @@
+                    <nav class="sidebar-nav">
+                        <ul class="metismenu" id="module-left-menu">
+                            <li class="sidebar-nav-heading">Navigasi</li>
+                            <li>
+                                <a href="dashboard">
+                                    <span class="sidebar-nav-item-icon fa fa-home fa-fw"></span>
+                                    <span class="sidebar-nav-item">Beranda</span>
+                                 </a>
+                            </li>
+                            <li>
+                                <a class="has-arrow" href="#" aria-expanded="false">
+                                    <span class="sidebar-nav-item-icon fa fa-shopping-cart fa-fw"></span>
+                                    <span class="sidebar-nav-item">Inventori</span>
+                                </a>
+                                <ul class="module-left-dropdown" aria-expanded="false">
+                                    <li><a href="/alkes">Data Obat & Alkes</a></li>
+                                    <li><a href="/nonAlkes">Data Non Alkes </a></li>
+                                </ul>
+                            </li>                           
+                            <li class="sidebar-nav-heading">Lain-lain</li>
+                            <li>
+                                <a href="login">
+                                    <span class="sidebar-nav-item-icon fa fa-sign-out fa-fw"></span>
+                                    <span class="sidebar-nav-item">Logout</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+    
