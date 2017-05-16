158ffb96cf0b84c2d7be704db1944416fc769835
syahril_rachman <rachmansyahril@gmail.com>
2017-05-16 19:38:11 +0700

    - Update alkes.blade - Update Pembelian Alkes (Tipe 1 transaksi)

--- public/views/pembelian_alkes/pembelian_alkes.blade.php
+++ public/views/pembelian_alkes/pembelian_alkes.blade.php
@@ -28,7 +28,7 @@
             <div class="col-md-12 no-padding m-b-15">
                 <button 
                     class="btn btn-info col-md-4 no-radius" 
-                    ng-click="openModal('credAlkesModal', 'tambah')">
+                    ng-click="openModal('createPOModal', 'tambah')">
                     Tambah PO
                 </button>
             </div>
@@ -68,6 +68,184 @@
             </div>
         </div>
     </div>
+    <script type="text/ng-template" id="createPOModal">
+        <div class="row p-b-15">
+            <h4 class="modal-title">Tambah PO</h4>
+        </div>
+        <div class="row p-b-15">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <div class="form-group field row text-left">
+                        <h4>Vendor</h4>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-4 no-padding">
+                            <b>Pilih Vendor</b>
+                        </div>
+                        <div class="col-sm-8">
+                            <select
+                                class="form-control m-b"
+                                ng-model="temp.vendor"
+                                ng-options="d as d.name for d in distributors">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left" ng-show="temp.vendor">
+                        <div class="col-sm-4 no-padding">
+                            <b>Nama Vendor</b>
+                        </div>
+                        <div class="col-sm-8">
+                            [[temp.vendor.name]]
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left" ng-show="temp.vendor">
+                        <div class="col-sm-4 no-padding">
+                            <b>Alamat Vendor</b>
+                        </div>
+                        <div class="col-sm-8">
+                            [[temp.vendor.address]]
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left" ng-show="temp.vendor">
+                        <div class="col-sm-4 no-padding">
+                            <b>Kontak Vendor</b>
+                        </div>
+                        <div class="col-sm-8">
+                            [[temp.vendor.phone]]
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group field row text-left">
+                        <h4>Ship To</h4>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-12">
+                            Nama Rumah Sakit
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row p-b-15">
+            <div class="col-md-12 no-padding">
+                <div class="sub-title col-md-12">
+                    <h4 class="text-left no-margin no-padding">[[pp.poly.name]]</h4>
+                </div>
+                <table class="table table-payment">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Item</th>
+                            <th class="text-center">Description</th>
+                            <th class="text-center">Qty</th>
+                            <th class="text-center">Unit Price</th>
+                            <th class="text-right">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr ng-repeat="p in pp.payments">
+                            <td class="text-center">[[$index + 1]]</td>
+                            <td class="text-center" ng-show="p.service.name">[[p.service.name]]</td>
+                            <td class="text-center" ng-hide="p.service.name">[[p.type]]</td>
+                            <td class="text-center" ng-show="p.service">[[p.total/p.service.cost]]</td>
+                            <td class="text-center" ng-hide="p.service">[[p.total/p.total]]</td>
+                            <td class="text-center" ng-show="p.service.cost">[[p.service.cost | currency]]</td>
+                            <td class="text-center" ng-hide="p.service.cost">[[p.total | currency]]</td>
+                            <td class="text-right">[[p.total | currency]]</td>
+                        </tr>
+                    </tbody>                    
+                </table>
+            </div>
+        </div>
+        <div class="row p-b-15">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <div class="form-group field row text-left">
+                        <h5>Komentar/Spesifikasi Lainnya</h5>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-12">
+                            Nama Rumah Sakit
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-4 no-padding">
+                            <b>Subtotal</b>
+                        </div>
+                        <div class="col-sm-8">
+                            <input 
+                                type="text" 
+                                class="form-control hidden-print"
+                                name="subtotal"
+                                ng-model="temp.subtotal">
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-4 no-padding">
+                            <b>Tax</b>
+                        </div>
+                        <div class="col-sm-8">
+                            <input 
+                                type="text" 
+                                class="form-control hidden-print"
+                                name="tax"
+                                ng-model="temp.tax">
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-4 no-padding">
+                            <b>Shipping</b>
+                        </div>
+                        <div class="col-sm-8">
+                            <input 
+                                type="text" 
+                                class="form-control hidden-print"
+                                name="shipping"
+                                ng-model="temp.shipping">
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-4 no-padding">
+                            <b>Other</b>
+                        </div>
+                        <div class="col-sm-8">
+                            <input 
+                                type="text" 
+                                class="form-control hidden-print"
+                                name="other"
+                                ng-model="temp.other">
+                        </div>
+                    </div>
+                    <div class="form-group field row text-left">
+                        <div class="col-sm-4 no-padding">
+                            <b>Total</b>
+                        </div>
+                        <div class="col-sm-8">
+                            Rp. 4.500.000,00
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row col-md-12 pull-right">
+            <div class="col-md-9">
+                <div class="bg-warning" style="min-height: 34px;"
+                    ng-show="message.crtPO.error">
+                    <p class="text-left">
+                        [[message.error]]
+                    </p>
+                </div>
+            </div>
+            <button
+                class="btn btn-info col-md-3 no-radius"
+                ng-show="typecredPO=='tambah'"
+                ng-click="createnewPO()">
+                Tambah
+            </button>
+        </div>
+    </script>
 @endsection
 @section('scripts')
 <script src="views/pembelian_alkes/pembelian_alkes.js"></script>
